c36d38afcb3c643f2a6965f5d830b7122117ad0a
Alemneh <Alemneh@hotmail.com>
2016-10-22 01:27:16 -0700

    initial commit

---
+++ models/index.js
@@ -0,0 +1,12 @@
+'use strict';
+var mongoose = require('mongoose');
+var env = process.env.NODE_ENV || 'devlopment';
+var CONFIG = require(__dirname +'/../config/config.json')[env];
+
+mongoose.connect(process.env[CONFIG.database] || CONFIG.database);
+const models = {};
+
+require('./user')(mongoose, models);
+
+
+module.exports = models;
---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict';
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const models = require('./models');
+const userRouter = express.Router();
+const env = process.env.NODE_ENV || 'devlopment';
+const CONFIG = require('./config/config.json')[env];
+const port = process.env.PORT || CONFIG.port || 3000;
+const url = process.env.URL || CONFIG.host || 'http://localhost:3000';
+
+
+
+require('./routes/users-routes')(userRouter, models);
+
+
+app.use(express.static(__dirname + '/build'));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', url);
+  res.header('Access-Control-Allow-Headers', 'Content-Type, authorization, token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
+
+app.use('/', userRouter);
+
+
+app.listen(port, () => {console.log('port up on '+ port);});
